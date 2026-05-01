8215e659dda01dc4c19c70472a6517ac6fc0e14b
Abrahim Nisar <rajaking11235@gmail.com>
2026-05-01 19:48:25 +0500

    Update Task1.cpp

--- Week 4/Task1.cpp
+++ Week 4/Task1.cpp
@@ -2,59 +2,57 @@
 #include <cstdlib>
 #include <string>
 #include <ctime>
+
 using namespace std;
 
-class Account{
-	private:
-		string accountNumber;
-		double balance;
-	public:
-		Account(string a="", double b=0){
-			accountNumber=a;
-			balance=b;
-		}
-		void getdata(){
-			cout<<"Enter your Account number: "<<endl;
-			cin>>accountNumber;
-			cout<<"Enter your balance: "<<endl;
-			cin>>balance;
-		}
-		int getrandom(){
-			return rand() % 1000;
-		}
-		Account operator+(const Account& obj)
-		{
-			Account k;
-			k.balance=this->balance + obj.balance;
-			k.accountNumber = "COMBINED" + getrandom();
-			return k;
-		}
-		void deposit(double amount){
-			if(amount > 0){
-				balance += amount;
-			}
-		}
-		void withdraw(double amount){
-			if(amount > 0 && amount <= balance){
-				balance -= amount;
-			}
-		}
-		void display(){
-			cout<<"Account Number :"<<accountNumber<<endl;
-			cout<<"Balance :"<<balance<<endl;
-		}
+class Account {
+private:
+    string accountNumber;
+    double balance;
+
+public:
+    Account(string a = "", double b = 0.0) {
+        if (a == "") {
+            accountNumber = to_string(rand() % 90000 + 10000);
+        } else {
+            accountNumber = a;
+        }
+        balance = b;
+    }
+    void getdata() {
+        cout << "Enter balance for account " << accountNumber << ": ";
+        cin >> balance;
+    }
+    Account operator+(const Account& obj) {
+        double newBalance = this->balance + obj.balance;
+        string newID = "Combined_" + this->accountNumber + "_" + obj.accountNumber;
+        return Account(newID, newBalance);
+    }
+
+    void display() {
+        cout << "Account Number: " << accountNumber << " | Balance: $" << balance << endl;
+    }
 };
-int main()
-{
-	srand(time(0));
-	Account a;
-	cout<<"----Account 1----"<<endl;
-	a.getdata();
-	a.display();
-	Account b("5466567",456);
-	cout<<"----Account 2----"<<endl;
-	b.display();
-	Account c= a + b;
-	cout<<"----Combined Account----"<<endl;
-	c.display();
+
+int main() {
+    srand(time(0));
+	
+    Account a; 
+    cout << "---- Account 1 (Randomly Generated) ----" << endl;
+    a.getdata();
+
+
+    Account b;
+    cout << "\n---- Account 2 (Randomly Generated) ----" << endl;
+    b.getdata();
+    Account c = a + b;
+
+    cout << "\n--- Summary ---" << endl;
+    a.display();
+    b.display();
+    cout << "---------------------------------------" << endl;
+    cout << "Combined Account Details:" << endl;
+    c.display();
+
+    return 0;
 }
